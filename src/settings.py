2e5a433440739bc40aa7a85dabbc15c143231481
unknown <you@example.com>
2021-08-30 04:57:05 +0600

    'commited'

--- src/settings.py
+++ src/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-(%zz+ztmyd5w%cd*_*7yqoel%6zcf4yisorft&*+3(_#ue)2vc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nayem-islam.herokuapp.com', 'localhost', '127.0.0.1:8000/']
 
 
 # Application definition
